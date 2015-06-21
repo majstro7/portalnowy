7ee01662ca3f6fe6166da7a096732bccdb302242
majstro7 <majstro7@gmail.com>
2015-06-21 15:24:14 +0200

    15:24

--- js/navigation-fixed.js
+++ js/navigation-fixed.js
@@ -3,9 +3,9 @@ $("document").ready(function($){
     var nav = $('.navbar.navbar-inverse.navbar-fixed-top');
     
     $(window).scroll(function () {
-        if ($(this).scrollTop() < 1) {
+        if ($(this).scrollTop() < 1 ) {
             nav.addClass("scrolling");
-        } else if($(this).scrollTop() >= 380){
+        } else if($(this).scrollTop() >= 460){
             nav.removeClass("scrolling");
         }
     });
